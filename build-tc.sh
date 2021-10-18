e6ca1f7422264e1f32d7c414bafa9ed2121f86b1
Finix-Projetcs <jarbull87@gmail.com>
2021-10-18 22:11:17 +0700

    Update build-tc.sh

--- build-tc.sh
+++ build-tc.sh
@@ -101,7 +101,7 @@ rm -fr ./*
 cp -r ../install/* .
 git checkout README.md # keep this as it's not part of the toolchain itself
 git add .
-git commit -asm "$LLVM_NAME: Toolchain Clang Bump to $rel_date Build
+git commit -asm "$LLVM_NAME: Toolchain Clang Bump to '$rel_date' Build of $llvm_commit_url
 
 LLVM commit: $llvm_commit_url
 Clang Version: $clang_version
