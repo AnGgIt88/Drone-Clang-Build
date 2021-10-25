aab3a98fc6e27b1f510494c60df26418d46c444b
AnGgIt88 <jarbull87@gmail.com>
2021-10-26 02:57:11 +0700

    update dependency and Cmake
    Signed-off-by: AnGgIt88 <jarbull87@gmail.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get -y update && apt-get -y upgrade
+apt-get -y install git cmake clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python python-clang build-essential make bzip2 libncurses5-dev lld libssl-dev ninja-build liblz4-tool bc curl unzip lzop ccache flex strace bison gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi
 apt-get update --fix-missing
 export ALLOW_MISSING_DEPENDENCIES=true
